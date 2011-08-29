f85fa213ce7bdc80fa6b6f5b112cd21b0cbcfb37
Raul Leal <leal.ra@gmail.com>
2011-08-29 12:05:24 +0200

    initial commit

---
+++ strTruncator.js
@@ -0,0 +1,54 @@
+/*!
+ * strTruncator plugin 
+ * for jQuery http://jquery.com/
+ *
+ * Copyright 2011, Raúl Leal Membrilla 
+ * http://rleal.com
+ *
+ * Licensed under the MIT license
+ */
+
+(function ($) {
+	$.fn.strTruncator = function(options) {
+		var defaults = {  
+			maxLength : 4,
+			background	: 'transparent',
+			opacity: 1,
+			offsetY: 0,
+			offsetX: 0,
+			endString: '...',
+        };
+
+        var options = $.extend(defaults, options);
+
+		return this.each(function() {  
+
+			var text = $(this).text();
+			
+			if ((text.length - options.endString.length) > options.maxLength ) {
+				$(this).text(text.substr(0, options.maxLength) + options.endString);
+				$(this).hover ( 
+					function() {
+						console.log($(this).css('line-height'))
+						var offset = $(this).offset();
+						var height = $(this).height();
+						var tooltip = $('<div class="strTruncator">'+text+'</div>').css({
+							display: 'none',
+							'background-color'	: options.background,
+							opacity	: "1", 
+							position	: 'absolute',
+							top		: offset.top + options.offsetY +'px',
+							left	: offset.left + options.offsetX +'px',
+							'line-height'	: height+'px', 
+						});
+						$(this).append(tooltip);
+						tooltip.show('fast');
+					},
+					function() {
+						$(this).find('div:last').remove();
+					}					
+				);
+			}
+		});
+	}
+})(jQuery);
